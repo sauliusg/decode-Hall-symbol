7920f3f33c940bfa4ddf1ebeb1c19c7950e35629
saulius <saulius@df35e32c-3005-4c95-812c-7079c7d8870e>
2022-06-07 15:02:15 +0000

    ada/ (saulius@tasmanijos-velnias) Refactoring the parser code; printing out matrices in a more compact form.
    git-svn-id: svn+ssh://saulius.grazulis.lt/home/saulius/svn-repositories/decode-Hall-symbol/trunk@5 df35e32c-3005-4c95-812c-7079c7d8870e

--- ada/decode-Hall-symbol/programs/decode_hall.adb
+++ ada/decode-Hall-symbol/programs/decode_hall.adb
@@ -1,3 +1,5 @@
+pragma Ada_2022;
+
 with Text_IO;             use Text_IO;
 with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
 with Ada.Command_Line;    use Ada.Command_Line;
@@ -33,17 +35,6 @@ procedure Decode_Hall is
       end loop;
    end;
    
-   procedure Init_Centering_Matrices 
-     (
-      Symbol : in String;
-      Pos : in out Positive;
-      Symops : in Symop_Array;
-      N_Symops : in out Positive
-     ) is
-   begin
-      null;
-   end;
-   
    procedure Skip_Spaces (S : in String; Pos : in out Integer ) is
    begin
       while Pos <= S'Last and then S (Pos) = ' ' loop
@@ -51,21 +42,43 @@ procedure Decode_Hall is
       end loop;
    end;
    
+   procedure Init_Global_Centering
+     (
+      Symbol : in String;
+      Pos : in out Positive;
+      Symops : in out Symop_Array;
+      N_Symops : in out Positive
+     ) is
+   begin
+      Skip_Spaces (Symbol, Pos);
+      if Symbol (Pos) = '-' then
+         Init_Ci (Symops (N_Symops));
+         N_Symops := N_Symops + 1;
+      end if;
+   end;
+   
+   procedure Init_Centering_Matrices 
+     (
+      Symbol : in String;
+      Pos : in out Positive;
+      Symops : in out Symop_Array;
+      N_Symops : in out Positive
+     ) is
+   begin
+      Skip_Spaces (Symbol, Pos);
+      null;
+   end;
+   
    function Decode_Hall (Symbol : in String) return Symop_Array is
       Max_Symops : constant Integer := 96;
       Symops : Symop_Array (1 .. Max_Symops);
-      N_Symops : Positive := 1;
-      Pos : Positive := 1; -- current position in the string 'Symbol'.
+      N_Symops : Positive := 2; -- The first element is allocated for the
+                                -- unity matrix.
+      Pos : Positive := 1;      -- current position in the string 'Symbol'.
    begin
       Init_Unity (Symops (1));
-      Skip_Spaces (Symbol, Pos);
-      if Symbol (Pos) = '-' then 
-         Init_Ci (Symops (2));
-         N_Symops := 2;
-      end if;
-      Skip_Spaces (Symbol, Pos);
+      Init_Global_Centering (Symbol, Pos, Symops, N_Symops);
       Init_Centering_Matrices (Symbol, Pos, Symops, N_Symops);
-      
       return Symops (1..N_Symops);
    end;
    
@@ -73,6 +86,23 @@ begin
    
    for I in 1 .. Argument_Count loop
       Put_Line (Argument (I));
+      declare
+         Symops : Symop_Array := Decode_Hall (Argument (I));
+      begin
+         for I in Symops'Range loop
+            declare
+               S : Symop := Symops (I);
+            begin
+               for J in Symop'Range(1) loop
+                  for K in Symop'Range(1) loop
+                     Put (" " & S (J,K)'Image);
+                  end loop;
+                  New_Line;
+               end loop;
+               New_Line;
+            end;
+         end loop;
+      end;
    end loop;
    
 end Decode_Hall;
