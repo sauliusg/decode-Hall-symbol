2c495ff72875cafa5a6e70806ca752a7844ccfd7
saulius <saulius@df35e32c-3005-4c95-812c-7079c7d8870e>
2022-06-08 14:26:41 +0000

    ada/ (saulius@tasmanijos-velnias) Removing old debug prints. Adding centering and inversion analysis.
    git-svn-id: svn+ssh://saulius.grazulis.lt/home/saulius/svn-repositories/decode-Hall-symbol/trunk@24 df35e32c-3005-4c95-812c-7079c7d8870e

--- ada/decode-Hall-symbol/programs/decode_hall.adb
+++ ada/decode-Hall-symbol/programs/decode_hall.adb
@@ -93,6 +93,26 @@ procedure Decode_Hall is
    Translations_6_4 : constant Crystallographic_Translation_Component := (4,6);
    Translations_6_5 : constant Crystallographic_Translation_Component := (5,6);
    
+   procedure Put (S : Symop) is
+   begin
+      for J in Symop'Range(1) loop
+         for K in Symop'Range(1) loop
+            Put (" " & S (J,K)'Image);
+         end loop;
+         New_Line;
+      end loop;
+   end;
+   
+   procedure Put (S : Crystallographic_Translation) is
+      Ratio : Float;
+   begin
+      for I in S'Range loop
+         Ratio := Float (S (I).Numerator) / Float (S (I).Denominator); 
+         Put (" " & Ratio'Image);
+      end loop;
+      New_Line;
+   end;
+   
    function To_Symop (T : Crystallographic_Translation) return Symop is
       S : Symop := Zero_Matrix;
    begin
@@ -257,16 +277,63 @@ procedure Decode_Hall is
       end loop;
    end;
    
-   procedure Init_Centering_Matrices 
+   procedure Get_Hall_Symbol_Inversions
+     (
+      Symbol : in String;
+      Pos : in out Integer;
+      N_Inversions : out Positive
+     )
+   is
+   begin
+      Skip_Spaces (Symbol, Pos);
+      if Symbol (Pos) = '-' then
+         N_Inversions := 2;
+         Pos := Pos + 1;
+      else
+         N_Inversions := 1;
+      end if;
+   end;
+   
+   procedure Get_Hall_Symbol_Centerings
      (
       Symbol : in String;
       Pos : in out Positive;
-      Symops : in out Symop_Array;
-      N_Symops : in out Positive
-     ) is
+      Centering : out Symop_Array;
+      N_Centering : out Positive
+     )
+   is
    begin
       Skip_Spaces (Symbol, Pos);
-      null;
+      Centering (1) := Unity_Matrix;
+      case Symbol (Pos) is
+         when 'P' =>
+           N_Centering := 1;
+         when 'A' =>
+           Centering (2) := To_Symop (A_Translation_Vector);
+           N_Centering := 2;
+         when 'B' =>
+           Centering (2) := To_Symop (B_Translation_Vector);
+           N_Centering := 2;
+         when 'C' =>
+           Centering (2) := To_Symop (C_Translation_Vector);
+           N_Centering := 2;
+         when 'I' =>
+           Centering (2) := To_Symop (I_Translation_Vector);
+           N_Centering := 2;
+         when 'F' =>
+           Centering (2) := To_Symop (F_Translation_Vector_1);
+           Centering (3) := To_Symop (F_Translation_Vector_2);
+           Centering (4) := To_Symop (F_Translation_Vector_3);
+           N_Centering := 4;
+         when 'R' =>
+           Centering (2) := To_Symop (R_Translation_Vector_1);
+           Centering (3) := To_Symop (R_Translation_Vector_2);
+           N_Centering := 3;
+         when others =>
+           Centering (1) := Unity_Matrix;
+           N_Centering := 1;           
+      end case;
+      Pos := Pos + 1;
    end;
    
    function Decode_Hall (Symbol : in String) return Symop_Array is
@@ -276,86 +343,36 @@ procedure Decode_Hall is
                                 -- unity matrix.
       Pos : Positive := 1;      -- current position in the string 'Symbol'.
       
-      N_Inversions : Positive := 1;
+      N_Inversions : Positive;
       
       Rotations : Symop_Array (1..3);
-      N_Rotations : Positive := 1;
+      N_Rotations : Positive;
       
-      Centering : Symop_Array (1..4) := (others => Unity_Matrix);
-      N_Centering : Positive := 1;
+      Centering : Symop_Array (1..4);
+      N_Centering : Positive;
       
    begin
       Symops (1) := Unity_Matrix;
+      
+      Get_Hall_Symbol_Inversions (Symbol, Pos, N_Inversions);
+      Get_Hall_Symbol_Centerings (Symbol, Pos, Centering, N_Centering);
+      
+      Put_Line ("Inversions:");
+      for I in 1..N_Inversions loop
+         Put (Inversion_Matrices (I));
+         New_Line;
+      end loop;
+      
+      Put_Line ("Centerings:");
+      for I in 1..N_Centering loop
+         Put (Centering (I));
+         New_Line;
+      end loop;
+      
       return Symops (1..N_Symops);
    end;
    
-   procedure Put (S : Symop) is
-   begin
-      for J in Symop'Range(1) loop
-         for K in Symop'Range(1) loop
-            Put (" " & S (J,K)'Image);
-         end loop;
-         New_Line;
-      end loop;
-   end;
-   
-   procedure Put (S : Crystallographic_Translation) is
-      Ratio : Float;
-   begin
-      for I in S'Range loop
-         Ratio := Float (S (I).Numerator) / Float (S (I).Denominator); 
-         Put (" " & Ratio'Image);
-      end loop;
-      New_Line;
-   end;
-   
 begin
-   
-   Put (A_Translation_Vector);
-   New_Line;
-   Put (B_Translation_Vector);
-   New_Line;
-   Put (C_Translation_Vector);
-   New_Line;
-   Put (I_Translation_Vector);
-   New_Line;
-   
-   Put_Line ("Translation Matrices:");
-   for I in 1..3 loop
-      Put (To_Symop (Translations_3_1, I));
-      New_Line;
-   end loop;
-   
-   for I in 1..3 loop
-      Put (To_Symop (Translations_6_1, I));
-      New_Line;
-   end loop;
-   
-   for I in 1..3 loop
-      Put (To_Symop (Translations_6_4, I));
-      New_Line;
-   end loop;
-   
-   for I in 1..3 loop
-      Put (To_Symop (Translations_6_5, I));
-      New_Line;
-   end loop;
-   
-   Put_Line ("Principal Rotations:");
-   for I in Principal_Rotations'Range(1) loop
-      for J in Principal_Rotations'Range(2) loop
-         Put (Principal_Rotations (I,J));
-         New_Line;         
-      end loop;
-   end loop;
-   
-   Put_Line ("Face Diagonal Rotations:");
-   for I in Face_Diagonal_Rotations'Range(1) loop
-      for J in Face_Diagonal_Rotations'Range(2) loop
-         Put (Face_Diagonal_Rotations (I,J));
-         New_Line;         
-      end loop;
-   end loop;
    
    for I in 1 .. Argument_Count loop
       Put_Line (Argument (I));
