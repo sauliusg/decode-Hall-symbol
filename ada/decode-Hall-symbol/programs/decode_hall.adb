12deb2cd56f2afcc820bc2981bbbdc2178a35bcc
saulius <saulius@df35e32c-3005-4c95-812c-7079c7d8870e>
2022-06-09 11:42:55 +0000

    ada/ (saulius@tasmanijos-velnias) Making sure that all tested symops for sg 230 fit the allocated symop array.
    git-svn-id: svn+ssh://saulius.grazulis.lt/home/saulius/svn-repositories/decode-Hall-symbol/trunk@42 df35e32c-3005-4c95-812c-7079c7d8870e

--- ada/decode-Hall-symbol/programs/decode_hall.adb
+++ ada/decode-Hall-symbol/programs/decode_hall.adb
@@ -671,7 +671,7 @@ procedure Decode_Hall is
    function Decode_Hall (Symbol : in String) return Symop_Array is
       Max_Symops : constant Integer := 96;
       
-      Symops : Symop_Array (1 .. Max_Symops);
+      Symops : Symop_Array (1 .. 2 * Max_Symops);
       N_Symops : Positive := 1;
       
       Pos : Positive := 1;      -- current position in the string 'Symbol'.
