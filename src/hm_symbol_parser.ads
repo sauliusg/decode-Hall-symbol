44be081412e658526b64ebc709a872096cfc0cb9
saulius <saulius@df35e32c-3005-4c95-812c-7079c7d8870e>
2024-11-01 16:35:37 +0000

    Removing duplicated code.
    trunk/ (saulius@starta)
    
    
    git-svn-id: svn+ssh://saulius.grazulis.lt/home/saulius/svn-repositories/decode-Hall-symbol/trunk@246 df35e32c-3005-4c95-812c-7079c7d8870e

--- src/hm_symbol_parser.ads
+++ src/hm_symbol_parser.ads
@@ -4,6 +4,8 @@ package HM_Symbol_Parser is
    
    Debug_Print_Matrices : Boolean := False;
    
+   HM_SYMBOL_NOT_FOUND : exception;
+   
    function Decode_Hermann_Mauguin_Symbol (Symbol : in String)
                                           return Symmetry_Operator_Array;
    
